7f1d60f1749035db6ffeb1f3fe40e89ed046b092
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2015-02-16 22:47:16 -0800

    adsf

--- question2_c.sql
+++ question2_c.sql
@@ -46,9 +46,9 @@ VALUES(4,200,40,5);
 INSERT INTO Y
 VALUES(4,200,50,10);
 
-INSERT INTO S
+INSERT INTO Z
 VALUES(2000,20,22);
-INSERT INTO S
+INSERT INTO Z
 VALUES(5000,50,55);
 
 /*
@@ -58,7 +58,66 @@ ii)		Deletion does NOT violate any integrity constraints
 
 iii)	Deletion does NOT violate any integrity constraints
 
-iv)	
+iv)		Deletion does NOT violate any integrity constraints
 
-V)			
+V)		Deletion does NOT violate any integrity constraints
+
+
+DATABASE AFTER UPDATES AND DELETIONS
+
+mysql> select * from R; select * from S; select * from T; select * from Y; select * from Z;
++------+----+-----+
+| A    | B  | C   |
++------+----+-----+
+| 1000 | 20 | 555 |
+| 3000 | 30 | 111 |
+| 4000 | 30 | 555 |
+| 5000 | 40 | 444 |
++------+----+-----+
+4 rows in set (0.00 sec)
+
++---+-----+------+
+| D | E   | F    |
++---+-----+------+
+| 2 | 100 | 3000 |
+| 2 | 200 | 2000 |
+| 3 | 100 | 5000 |
+| 3 | 200 | 2000 |
+| 4 | 200 | 1000 |
++---+-----+------+
+5 rows in set (0.00 sec)
+
++----+-----+-----+
+| G  | H   | I   |
++----+-----+-----+
+| 10 |  50 | 100 |
+| 20 | 125 | 200 |
+| 40 |  75 | 400 |
+| 50 | 100 | 200 |
++----+-----+-----+
+4 rows in set (0.00 sec)
+
++---+-----+----+----+
+| M | N   | O  | P  |
++---+-----+----+----+
+| 1 | 100 | 10 | 25 |
+| 1 | 100 | 20 |  5 |
+| 2 | 100 | 20 | 20 |
+| 2 | 200 | 20 | 15 |
+| 3 | 100 | 40 | 15 |
+| 4 | 100 | 50 | 35 |
+| 4 | 200 | 40 |  5 |
+| 4 | 200 | 50 | 10 |
++---+-----+----+----+
+8 rows in set (0.00 sec)
+
++------+----+----+
+| J    | K  | L  |
++------+----+----+
+| 2000 | 20 | 22 |
+| 5000 | 20 | 75 |
+| 5000 | 50 | 55 |
++------+----+----+
+3 rows in set (0.00 sec)
+
 */
