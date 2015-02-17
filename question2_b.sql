d64735b1cf4250294d62a3def38bae134585ece8
unknown <jeremy.martinez@is-082.ad.wsu.edu>
2015-02-16 16:00:22 -0800

    q2 (b) done

---
+++ question2_b.sql
@@ -0,0 +1,9 @@
+/*
+i)		this would NOT violate any primary keys or key constraints
+
+ii)		this would NOT violate any primary keys or key constraints
+
+iii)	this would NOT violate any primary keys or key constraints
+
+iv)		this would violate a primary key because there already exists an entry with T(G) = 20
+*/
