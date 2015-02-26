e844f3d74206ba69670b8dd848e6f94bae3398a3
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2015-02-25 23:34:16 -0800

    removed hw2 stuff on this branch

--- question2_b.sql
+++
@@ -1,9 +0,0 @@
-/*
-i)		this would NOT violate any primary keys or key constraints
-
-ii)		this would NOT violate any primary keys or key constraints
-
-iii)	this would NOT violate any primary keys or key constraints
-
-iv)		this would violate a primary key because there already exists an entry with T(G) = 20
-*/
